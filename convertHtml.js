7f79f500766a1977e02ab54fe89cb95ed3a31874
Prasham <prasham.trivedi@dynamicelements.no>
2020-08-30 17:32:08 +0530

    Adding Search

--- convertHtml.js
+++ convertHtml.js
@@ -1,5 +1,5 @@
 const showdown = require('showdown')
-
+const elasticlunr = require('elasticlunr')
 
 const plugin = {
     type: 'output',
@@ -8,27 +8,39 @@ const plugin = {
 }
 const converter = new showdown.Converter({tables: true, strikethrough: true, openLinksInNewWindow: true, metadata: true, extensions: [plugin]})
 const fs = require('fs')
-
+const index = elasticlunr(function () {
+    this.addField('title')
+    this.addField('body')
+    this.addField('fileName')
+})
 
 module.exports.exportHtml = () => {
     const currentDir = process.cwd()
     const htmlPath = `${currentDir}/html/`
     const filesArr = []
 
+    let currentId = 0
     fs.readdirSync(currentDir).filter(file => file.endsWith('.md') && !file.startsWith('readme')).forEach(file => {
         const fileData = fs.readFileSync(file).toString()
-
 
         let htmlData = converter.makeHtml(fileData)
         htmlData = htmlData.split(".md").join(".html")
         const metadata = converter.getMetadata()
         const title = metadata.title
+
         if (!fs.existsSync(htmlPath)) {
             fs.mkdirSync(htmlPath)
         }
-        fs.copyFileSync('./style.css', `${htmlPath}/style.css`)
-        fs.copyFileSync('./functions.js', `${htmlPath}/functions.js`)
+
         const fileName = `${file.replace('.md', '.html')}`
+        const doc = {
+            id: currentId,
+            title,
+            body: htmlData,
+            fileName
+        }
+        index.addDoc(doc)
+        currentId = currentId + 1
         filesArr.push(`
         <li class="navLinks">
             <div>
@@ -42,6 +54,8 @@ module.exports.exportHtml = () => {
                                 <title>
                                 ${title}
                                 </title>
+                                <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+                                <meta content="utf-8" http-equiv="encoding">
                                 <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
                                 <link rel="stylesheet" href="style.css">
                                 <script src="functions.js"></script>
@@ -61,26 +75,43 @@ module.exports.exportHtml = () => {
                     `
         fs.writeFileSync(`${htmlPath}/${fileName}`, post)
     })
-    const index = `
+    fs.copyFileSync('./style.css', `${htmlPath}/style.css`)
+    fs.copyFileSync('./functions.js', `${htmlPath}/functions.js`)
+    fs.copyFileSync('./node_modules/elasticlunr/elasticlunr.min.js', `${htmlPath}/elasticlunr.min.js`)
+
+    const indexFile = `
     <html>
         <head>
          <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="referrer" content="no-referrer">
-            <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="style.css">
-            <script src="functions.js"></script>
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+        <meta content="utf-8" http-equiv="encoding">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <script src="functions.js"></script>
+        <script src="elasticlunr.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script type="text/javascript" src="search.js"></script>
         </head>
         <body> 
-            <h1>Welcome to Prasham's notes.</h1>
+        <h1>Welcome to Prasham's notes.</h1>
+       <div class="searchBox">
+            <input type="text" id="search" placeholder="Search.." name="search">
+            <button type="submit" onClick=search()><i class="fa fa-search"></i></button>
+            <div id="searchResults" class="searchResults">
+            </div> 
+       </div>
             <ul>
                 ${filesArr.join(`\n`)}
             </ul>
         </body>
     </html>
     `
-    fs.writeFileSync(`${htmlPath}/index.html`, index)
+    fs.writeFileSync(`${htmlPath}/index.html`, indexFile)
+
+    fs.writeFileSync(`${htmlPath}/search.js`, `searchData = ${JSON.stringify(index)}`)
 
     const error = `
     <html>
--- functions.js
+++ functions.js
@@ -1,6 +1,37 @@
-function copyId(id) {
 
-    window.location.hash = `#${id}`
 
+let searchIndex = undefined
+function search() {
+    console.log(searchData)
+    if (!searchIndex) {
+        searchIndex = elasticlunr.Index.load(searchData)
+    }
+
+    var searchResultDiv = document.getElementById('searchResults')
+
+    const currentText = document.getElementById('search').value
+
+    const searchResult = searchIndex.search(currentText)
+    console.log(searchResult)
+    const search = []
+
+    for (const result of searchResult) {
+        const searchRef = searchData.documentStore.docs[result.ref]
+        if (searchRef) {
+            search.push(` <li class="navLinks">
+            <div>
+             <a href="./${searchRef.fileName}" target="_blank">
+             ${searchRef.title}
+            </a>
+            </div>
+        </li>`)
+        }
+    }
+    searchResultDiv.innerHTML = `
+    <ul>
+        ${search.join('\n')}
+    </ul>`
 
 }
+
+
